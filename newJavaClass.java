11448ab82597895589fcf1f2258e300c46ad15c1
Anzar Shaikh <aznzarshaikh373@gmail.com>
2021-06-21 14:20:30 +0530

    See what happens

--- newJavaClass.java
+++ newJavaClass.java
@@ -3,5 +3,7 @@ package com.company;
 public class newJavaClass {
     public static void main(String[] args) {
         System.out.println("This is new class. ");
+        System.out.println("i am adding main also ");
+
     }
 }
